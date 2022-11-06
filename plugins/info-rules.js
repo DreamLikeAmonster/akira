59772ad5482cfad0a4afd2d35d1cb7cb7c864ae7
DreamLikeAmonster <89921736+DreamLikeAmonster@users.noreply.github.com>
2022-11-07 04:29:52 +0700

    Create info-rules.js

--- plugins/info-rules.js
+++ plugins/info-rules.js
@@ -33,13 +33,13 @@ let rules = `➯ *Tɪᴅᴀᴋ Sᴘᴀᴍ Bᴏᴛ*
 └─┈⟅`
 let nth = `☰⟥⟝⟞⟝❨ *Rᴜʟᴇs Mʏ Bᴏᴛ* ❩⟞⟝⟞⟤☰`
 conn.send3ButtonImg(m.chat, `${pickRandom(flaaa2)}` + `${ucapan()} ` + `${name}`, nth, rules, 'Menu', '.menu', 'Owner', '.owner', 'Credit', '.credit', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: 'https://wa.me/p/5489299044451325/6281361281833',
+    mediaUrl: 'https://wa.me/p/8038197776222653/62858425054589',
     mediaType: 2, 
     description: sgc,
-    title: "Sᴇᴡᴀ Cᴇᴋ Dɪsɪɴɪ Cᴜʏ!!!",
+    title: "Sᴇᴡᴀ Cᴇᴋ Dɪsɪɴɪ!!!",
     body: wm,
     thumbnail: fs.readFileSync('./media/qr.png'),
-    sourceUrl: 'https://wa.me/p/5489299044451325/6281361281833',
+    sourceUrl: 'https://wa.me/p/8038197776222653/62858425054589',
      }}
   })
 }
