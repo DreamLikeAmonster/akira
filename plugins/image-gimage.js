77fbfaeecbadec23d6892dc8784a6e20fa192e29
DreamLikeAmonster <89921736+DreamLikeAmonster@users.noreply.github.com>
2022-11-07 08:01:12 +0700

    Add files via upload

--- plugins/image-gimage.js
+++ plugins/image-gimage.js
@@ -1,13 +1,14 @@
 import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
+    if (!text) throw `ᴜꜱᴇ ᴇxᴀᴍᴘʟᴇ ${usedPrefix}ɪᴍᴀɢᴇ ꜰɪʟʟᴏ ᴄʜᴀɴ`
     const res = await googleImage(text)
     let image = res.getRandom()
     let link = image
     conn.sendHydrated(m.chat,`
-*${htki} GOOGLE IMAGE ${htka}*
+*${htki} AKIRA IMAGE ${htka}*
 🔎 *Result:* ${text}
-🌎 *Source:* Google
+🌎 *Source:* ᴀᴋɪʀᴀ.ᴄᴀʀᴇ
+⛩ *ɴᴀᴍᴇ ᴜꜱᴇʀ:* ${conn.getName(m.sender)}
 `, wm, link, link, '🔗 URL', null, null, [['Next', `.image ${text}`],[null,null],[null,null]],m)
 }
 handler.help = ['gimage <query>', 'image <query>']
